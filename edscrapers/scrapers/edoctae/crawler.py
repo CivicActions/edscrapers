45d881e44efbcd2bc9bac073121b55c87d24f6ff
higorspinto <higorspinto@gmail.com>
2020-02-27 15:10:25 -0300

    [update][s] Updating crawler. ref #4

--- edscrapers/scrapers/edoctae/crawler.py
+++ edscrapers/scrapers/edoctae/crawler.py
@@ -1,3 +1,4 @@
+import re
 from scrapy.spiders import Rule
 from scrapy.spiders import CrawlSpider
 from scrapy.linkextractors import LinkExtractor
@@ -6,7 +7,8 @@ from edscrapers.scrapers.edoctae.parser import parse
 class Crawler(CrawlSpider):
 
     name = 'edoctae'
-    allowed_regex = r'www2.ed.gov/about/offices/list/ovae/|sites.ed.gov/octae/'
+    allowed_regex = r'ovae/|octae/'
+    #|sites.ed.gov/octae/'
 
     def __init__(self, conf=None):
 
@@ -14,15 +16,25 @@ class Crawler(CrawlSpider):
 
         self.start_urls = [
             'https://www2.ed.gov/about/offices/list/ovae/index.html',
-            'https://sites.ed.gov/octae/'
+            'https://www2.ed.gov/about/offices/list/ovae/pi/memoperkinsiv.html',
         ]
 
         # Make rules
         self.rules = [
             Rule(LinkExtractor(
                 allow=self.allowed_regex,
+                deny='.*(xls|xlsx|csv|zip|pdf|doc|docx)',
+                #restrict_xpaths='//*[@id="maincontent"]',
+                process_value=self.process_value
             ), callback=parse, follow=True),
         ]
 
         # Inherit parent
         super(Crawler, self).__init__()
+
+    def process_value(self, value):
+        m = re.search("javascript:goToPage\('(.*?)'", value)
+        if m:
+            return m.group(1)
+        else:
+            return value
--- edscrapers/scrapers/edoctae/parser.py
+++ edscrapers/scrapers/edoctae/parser.py
@@ -1,40 +1,40 @@
-from .model import Model
+import json
+import edscrapers.scrapers.base.helpers as h
+
+from .model import Dataset
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 
 extensions = {
     '.xls': 'Excel',
     '.xlsx' : 'Excel',
-    '.pdf': 'PDF',
     '.zip': 'ZIP',
     '.csv': 'CSV',
 }
 
 def parse(res):
 
-    if 'finaid' in res.url:
-        print('FINAID!!')
-
     print(res)
-    
-    dataset = Model()
 
-    with open('edoctae.log', 'a') as result_log:
-        result_log.write(str(res) + '\n')
-        for link in LxmlLinkExtractor(allow=(),deny_extensions=[]).extract_links(res):       
-            for extension in extensions.keys():
-                if link.url.endswith(extension):
-                    resource = {
-                        'url': link.url,
-                        'name': link.text,
-                    }
-                    dataset.add_resource(resource)
-                    #print('{} file: {}'.format(extensions[extension], link.url))
-                    result_log.write('{}\n'.format(link.url))
+    dataset = Dataset()
+
+    for link in LxmlLinkExtractor(deny_extensions=[]).extract_links(res):
+        for extension in extensions.keys():
+            if link.url.endswith(extension):
+                resource = {
+                    'source_url': res.url,
+                    'url': link.url,
+                    'name': link.text,
+                }
+                dataset.add_resource(resource)
 
     if len(dataset.resources) > 0:
         dataset.source_url = res.url
-        dataset.title = res.xpath('/html/head/meta[@name="title"]/@content')
-        #dataset.dump()
-        return dataset
+        dataset.title = res.xpath('//meta[@name="DC.title"]/@content').get('text')
+        if not dataset.title or dataset.title == 'text':
+            dataset.title = res.xpath('/html/head/title/text()').get('text')
+        dataset.name = h.make_slug(res.url)
+        dataset.notes = res.xpath('//meta[@name="DC.description"]/@content').get('text')
+        dataset.dump()
+        return json.loads(dataset.toJSON())
 
     return None
