e513d1ec316814533f940038508e2ff2175755a3
higorspinto <higorspinto@gmail.com>
2020-03-02 14:50:51 -0300

    [wip][m] - Adding crawler for edosers (P5 Crawling). ref #7

---
+++ edscrapers/scrapers/edosers/crawler.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import Rule
+from scrapy.spiders import CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+
+from edscrapers.scrapers.edosers.parser import parse
+from edscrapers.scrapers.base import helpers as h
+
+class Crawler(CrawlSpider):
+
+    name = 'edosers'
+    allowed_regex = r'osers|osep|idea'
+    allowed_domains = ['www2.ed.gov','osep.grads360.org']
+
+    def __init__(self, conf=None):
+
+        self.conf = conf
+
+        self.start_urls = [
+            'https://www2.ed.gov/about/offices/list/osers/index.html',
+            'https://www2.ed.gov/about/offices/list/osers/products/employmentguide/index.html'
+        ]
+
+        # Make rules
+        self.rules = [
+            Rule(LinkExtractor(
+                allow=self.allowed_regex,
+                #deny=f'.*({"|".join(h.get_data_extensions())})',
+                #restrict_xpaths='//*[@id="maincontent"]'
+            ), callback=parse, follow=True),
+        ]
+
+        # Inherit parent
+        super(Crawler, self).__init__()
---
+++ edscrapers/scrapers/edosers/parser.py
@@ -0,0 +1,32 @@
+import json
+import edscrapers.scrapers.base.helpers as h
+from edscrapers.scrapers import base
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from edscrapers.scrapers.base.models import Dataset, Resource
+
+deny_list = []
+
+def parse(res):
+
+    print(res)
+
+    dataset = Dataset()
+    dataset.crawler_name = globals()['__package__'].split('.')[-1]
+
+    h.get_all_resources(res, dataset, h.get_data_extensions(), deny_list=deny_list)
+
+    if len(dataset.resources) > 0:
+
+        # We've got resources, so the documents might be relevant to them
+        h.get_all_resources(res, dataset, h.get_document_extensions(), deny_list=deny_list)
+
+        dataset.source_url = res.url
+        dataset.title = res.xpath('//meta[@name="DC.title"]/@content').get('text')
+        if not dataset.title or dataset.title == 'text':
+            dataset.title = res.xpath('/html/head/title/text()').get('text')
+        dataset.name = h.make_slug(res.url)
+        dataset.notes = res.xpath('//meta[@name="DC.description"]/@content').get('text')
+        dataset.dump()
+        return json.loads(dataset.toJSON())
+
+    return None
