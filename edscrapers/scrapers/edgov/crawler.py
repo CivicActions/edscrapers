b9651511cfdd51fbcdc51db64468c07d850d332b
osahon okungbowa <osahon.okungbowa@datopian.com>
2020-05-08 17:22:41 +0100

    [edgov-comparison][no-merge]: do not merge branch. Branch compares edgov datajson with scraper resource findings

--- edscrapers/scrapers/edgov/crawler.py
+++ edscrapers/scrapers/edgov/crawler.py
@@ -8,15 +8,27 @@ from scrapy.linkextractors import LinkExtractor
 from edscrapers.scrapers.edgov.parser import parse
 from edscrapers.scrapers.base import helpers as h
 
+import json
+import requests
+from pathlib import Path
+import os
+
 
 class Crawler(CrawlSpider):
 
     name = 'edgov'
 
+    # get the output path for comparison work
+    comparison_output_path = Path(os.getenv('ED_OUTPUT_PATH'), "ed_comparison")
+    # make the path
+    comparison_output_path.mkdir(parents=True, exist_ok=True)
+
     # allowed_regex = r'^http.*://[w2\.]*ed\.gov/.*$'
     allowed_domains = ['ed.gov', 'www2.ed.gov']
 
     def __init__(self):
+
+        self.load_ed_gov_json()
 
         self.start_urls = [
             'https://www2.ed.gov/finaid/prof/resources/data/teach-institution.html',
@@ -29,11 +41,43 @@ class Crawler(CrawlSpider):
             Rule(LinkExtractor(
                 #allow=self.allowed_regex,
                 deny_extensions=[regex[1:] for regex in h.get_data_extensions().keys()],
-                deny_domains=h.retrieve_crawlers_allowed_domains(except_crawlers=['edgov'])
+                #deny_domains=h.retrieve_crawlers_allowed_domains(except_crawlers=['edgov'])
                 #restrict_xpaths='//*[@id="maincontent"]',
-            ), callback=parse, follow=True),
+            ), callback=parse, follow=True, 
+               cb_kwargs={'distribution_list': self.dataset_distributions_list}),
         ]
 
 
         # Inherit parent
         super(Crawler, self).__init__()
+    
+    @classmethod
+    def load_ed_gov_json(cls):
+        
+        req = requests.get("https://www2.ed.gov/data.json", verify=False)
+
+        # dump the returned datajson to a file
+        with open(Path(cls.comparison_output_path, "ed_data.json"), 'w') as file:
+            json.dump(req.json(), file, indent=2, sort_keys=False)
+        
+        # add the returned json object to the class
+        cls.ed_json = req.json()
+        
+        # create the list of dataset distributions from the edgov datajson
+        cls.reduce_datasets_distributions()
+    
+    @classmethod
+    def reduce_datasets_distributions(cls):
+        
+        distributions_list = []
+        if not cls.ed_json:
+            raise AttributeError("class attribute 'ed_json' does not exist")
+
+        for dataset in cls.ed_json['dataset']:
+            distributions_list.extend(dataset.get('distribution', []))
+        
+        cls.dataset_distributions_list = distributions_list
+
+        with open(Path(cls.comparison_output_path, "ed_distributions.json"), 'w') as file:
+            json.dump(cls.dataset_distributions_list, file, indent=2, sort_keys=False)
+            
--- edscrapers/scrapers/edgov/parser.py
+++ edscrapers/scrapers/edgov/parser.py
@@ -11,27 +11,70 @@ import bs4
 from edscrapers.scrapers import base
 import edscrapers.scrapers.base.parser as base_parser
 from edscrapers.scrapers.edgov import parsers
+from edscrapers.scrapers.base.models import Dataset, Resource
+from slugify import slugify
+
+import urllib.parse
+
+class Distribution():
+
+    def __init__(self, distributions_list, source_url):
+        self.distributions_list = distributions_list
+        self.source_url = source_url
+    
+    def resource_checker(self, tag_attr: str):
+        """ function is used as a filter for BeautifulSoup to
+        locate resource links """
+
+        if tag_attr != '' or tag_attr is None:
+            return False
+
+        if urllib.parse.urlparse(tag_attr).scheme:
+            href = tag_attr
+        else:
+            href = urllib.parse.urljoin(self.source_url, tag_attr)
+        
+        for distribution_json in self.distributions_list:
+            if href.lower() == distribution_json.get('downloadURL', '').lower():
+                return True
+        return False
 
 
-def parse(res):
+
+def parse(res, distribution_list):
     """ function parses content to create a dataset model
     or return None if no resource in content"""
 
-    soup_parser = bs4.BeautifulSoup(res.text, 'html5lib')
-    # check if the content contains any of the extensions
-    if soup_parser.body.find(name='a', href=base_parser.resource_checker,
-                             recursive=True) is None:
-        # no resource on this page, so return None
-        return None
-    # if code gets here, at least one resource was found
-    
-    # check if the parser is working on EDGOV web page
-    if soup_parser.body.find(name='div', recursive=True) is not None:
-        # parse the page with the parser and return result
-        return parsers.parser1.parse(res)
-    else:
+    # ensure that the response text gotten is a string
+    if not isinstance(getattr(res, 'text', None), str):
         return None
 
+    try:
+        soup_parser = bs4.BeautifulSoup(res.text, 'html5lib')
+    except:
+        return None
+    
+    # create a Distribution class
+    distribution = Distribution(distribution_list, res.url)
+
+    # check if any resource on this page matches the urls in distribution list
+    resource_list = soup_parser.body.find_all(name='a', href=distribution.resource_checker, recursive=True)
+
+    for resource in resource_list:
+        dataset = Dataset()
+        # create dataset model dict
+        dataset = Dataset()
+        dataset['source_url'] = res.url
+
+        dataset['title'] = str(soup_parser.head.\
+                                find(name='title').string).strip()
+
+        # replace all non-word characters (e.g. ?/) with '-'
+        dataset['name'] = slugify(dataset['title'])
+
+        dataset['resource_url'] = resource['href']
+
+        yield dataset
 
     
 
