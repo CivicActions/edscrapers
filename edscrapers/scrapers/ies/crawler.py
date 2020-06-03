b923483c516a12f69dbb62e144f0fabebe545a48
Georgiana Bere <georgiana.bere@datopian.com>
2020-06-03 09:22:49 +0200

    Add ies scraper forked from nces

---
+++ edscrapers/scrapers/ies/crawler.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import re
+
+from scrapy.spiders import Rule
+from scrapy.spiders import CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+
+from edscrapers.scrapers.ies.parser import parse
+from edscrapers.scrapers.base import helpers as h
+
+
+class Crawler(CrawlSpider):
+
+    name = 'ies'
+
+    allowed_regex = r'(nces|ies)\.ed\.gov'
+    # Only nces has a dedicated website. All the other centers are subsections of `ies.ed/gov`
+    allowed_domains = ['nces.ed.gov','ies.ed.gov', ]
+
+    # TODO: Get the name of the suboffice from the publisher meta tag and ask Victor how to save these attributes
+    def __init__(self):
+
+        self.start_urls = [
+            'https://ies.ed.gov/pubsearch/',
+            'https://ies.ed.gov/resourcesforresearchers.asp',
+            # Imported from the nces parser
+            'https://nces.ed.gov/datatools/',
+            'https://ies.ed.gov/data.asp',
+            'https://nces.ed.gov/pubsearch/index.asp',
+            'https://nces.ed.gov/Datalab/TablesLibrary',
+            #'https://nces.ed.gov/pubs2009/expenditures/tables/table_08.asp?referrer=report'
+            #'https://nces.ed.gov/surveys/els2002/tables/APexams_01.asp'
+            #'https://nces.ed.gov/ipeds/deltacostproject/'
+            #'https://nces.ed.gov/pubs2009/expenditures/tables.asp'
+        ]
+
+        # Make rules
+        self.rules = [
+            Rule(LinkExtractor(
+                allow=self.allowed_regex,
+                deny=["\\" + regex for regex in h.get_data_extensions().keys()],
+                # restrict_xpaths='//*[@id="maincontent"]'
+                # process_value=lambda value: value.replace('http', 'https', 1),
+            ), callback=parse, follow=True),
+        ]
+
+
+        # Inherit parent
+        super(Crawler, self).__init__()
---
+++ edscrapers/scrapers/ies/parsers/__init__.py
@@ -0,0 +1,3 @@
+# import modules from this package with simplier names
+from edscrapers.scrapers.ies.parsers import nces_parser1
+from edscrapers.scrapers.ies.parsers import nces_parser2
