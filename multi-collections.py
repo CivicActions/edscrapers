a03181a99d0c9c6b3bb470aa5391a3b4c71e11c8
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-17 17:10:08 +0100

    outputting to csv

--- multi-collections.py
+++ multi-collections.py
@@ -37,13 +37,31 @@ def get_all_collection_graph_output_file(dir_path=OUTPUT_PATH) -> list:
     
     return collections_graph_files
 
-def identify_datasets_with_multi_collections(graph):
+def identify_datasets_with_multi_collections(graph_file_path):
     """ function identify datasets with multiple collections """
 
-    if isinstance(graph, (str, pathlib.Path)):
+    if isinstance(graph_file_path, (str, pathlib.Path)):
         # load the graph from the filepath provided
-        GraphWrapper.load_graph(file_dir_path=pathlib.Path(graph).parent,
-                                file_stem_name=pathlib.Path(graph).stem)
+        GraphWrapper.load_graph(file_dir_path=pathlib.Path(graph_file_path).parent,
+                                file_stem_name=pathlib.Path(graph_file_path).stem)
         # get the loaded graph object
-        
+        graph = GraphWrapper.get_graph()
 
+        with graph.graph_lock:
+            # select datasets vertices that are in multiple collections
+            dataset_ver_seq = graph.vs.select(is_dataset_eq=True, name_ne='base_vertext').\
+                select(lambda vertex: 'in_collection' in vertex.attribute_names() and vertex['in_collection'] is not None and len(vertex['in_collection']) > 1)
+            
+            # get the name of the office this graph belongs to
+            office_name = pathlib.Path(graph_file_path).stem.split('.')[0]
+            # info user that there are datasets with multiple collections
+            print(f'There are {len(dataset_ver_seq)} datasets with links to multiple Collections within the {office_name.upper()} office')
+            
+            return dataset_ver_seq
+                
+
+def output_to_csv(graph_vertex_seq):
+    """ function dumps the identified dataset vertices that
+    have multiple collections to a csv"""
+
+    
