9b1843ade0d5921bf8c3bafd1b1c5f9feb30a146
Osahon Okungbowa <osahon.okungbowa@datopian.com>
2020-06-19 13:35:05 +0100

    multiple collections datasets done

--- multi-collections.py
+++ multi-collections.py
@@ -9,7 +9,7 @@ varialbe same as the edscrapers toolkit """
 import os
 import pathlib
 
-import igraph
+import pandas as pd
 from edscrapers.scrapers.base.graph import GraphWrapper
 
 OUTPUT_PATH = os.getenv("ED_OUTPUT_PATH")
@@ -19,8 +19,12 @@ def get_all_collection_graph_output_file(dir_path=OUTPUT_PATH) -> list:
     path to each collection graph output i.e.
     files that end with {name}.collection.pickle """
 
-    # get the directory where all graph files are stored
-    graph_dir_path = pathlib.Path(dir_path, 'graphs')
+    if str(dir_path) == OUTPUT_PATH:
+        # get the directory where all graph files are stored
+        graph_dir_path = pathlib.Path(dir_path, 'graphs')
+    else:
+        graph_dir_path = pathlib.Path(dir_path)
+
     if not graph_dir_path.is_dir():
         raise TypeError("expected a directory path")
 
@@ -54,14 +58,46 @@ def identify_datasets_with_multi_collections(graph_file_path):
             
             # get the name of the office this graph belongs to
             office_name = pathlib.Path(graph_file_path).stem.split('.')[0]
+            dataset_ver_seq['office_name'] = office_name
             # info user that there are datasets with multiple collections
             print(f'There are {len(dataset_ver_seq)} datasets with links to multiple Collections within the {office_name.upper()} office')
             
             return dataset_ver_seq
                 
 
-def output_to_csv(graph_vertex_seq):
+def output_to_csv(graph_vertex_seq, graph_office_name):
     """ function dumps the identified dataset vertices that
     have multiple collections to a csv"""
 
+    if len(graph_vertex_seq) == 0: #the sequence is empty so set some defaults
+        graph_vertex_seq['collection_urls'] = ''
+        graph_vertex_seq['collection_names'] = ''
+        graph_vertex_seq['num_of_collection'] = 0
+
+    for vertex in graph_vertex_seq:
+        vertex['collection_urls'] = "\n".join([collection['collection_url'] for collection in vertex['in_collection']])
+        vertex['collection_names'] = "\n".join([collection['collection_title'] for collection in vertex['in_collection']])
+        vertex['num_of_collection'] = len(vertex['in_collection'])
+
+    # convert the vertex sequence to a panda frame
+    df = pd.DataFrame(columns=['Dataset URL', 'Dataset Office', 'Number of Collections Linked To', 'Collection URLs'])
+    df['Dataset URL'] = graph_vertex_seq['name']
+    df['Dataset Office'] = graph_vertex_seq['office_name']
+    df['Number of Collections Linked To'] = graph_vertex_seq['num_of_collection']
+    df['Collection URLs'] = graph_vertex_seq['collection_urls']
+
+    df.to_csv(pathlib.Path(OUTPUT_PATH, f'{graph_office_name}_dataset_multi_collection.csv'),
+                columns=['Dataset URL', 'Dataset Office', 'Number of Collections Linked To', 'Collection URLs'],
+                header=True, index=False)
+
+
+if __name__ == "__main__":
     
+    graph_file_list = get_all_collection_graph_output_file()
+
+    # loop through the graph file paths provided
+    for graph_file in graph_file_list:
+        name_of_office = pathlib.Path(graph_file).name.split('.')[-3]
+        v_sequence = identify_datasets_with_multi_collections(graph_file)
+        output_to_csv(graph_vertex_seq=v_sequence,
+                      graph_office_name=name_of_office)
