2ce2fb5e3e08d65deb1cac7e2621b2b0075aa822
higorspinto <higorspinto@gmail.com>
2020-07-23 13:21:59 -0300

    Improve dashboard.ed crawler.
    - remove useless parameters from urls

--- edscrapers/scrapers/dashboard/crawler.py
+++ edscrapers/scrapers/dashboard/crawler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 
+from urllib.parse import urlparse, urlunparse, urljoin
 from scrapy.spiders import Rule
 from scrapy.spiders import CrawlSpider
 from scrapy.linkextractors import LinkExtractor
@@ -31,6 +32,8 @@ class Crawler(CrawlSpider):
             Rule(LinkExtractor(
                 allow=self.allowed_regex,
                 deny_extensions=[ext[1:] for ext in extensions_to_avoid],
+                process_value=self.process_value,
+                unique=True,
                 deny_domains=h.retrieve_crawlers_allowed_domains(except_crawlers=['edgov'])
             ), callback=parse, follow=True),
         ]
@@ -38,3 +41,22 @@ class Crawler(CrawlSpider):
 
         # Inherit parent
         super(Crawler, self).__init__()
+
+    def process_value(self, value):
+        
+        url = value
+
+        if re.match(self.allowed_regex, url) is None:
+            return None
+
+        url_parsed = urlparse(url)
+        query = url_parsed.query
+
+        regex = r'&id=\d{1,3}&wt=\d{1,3}'
+        search = re.search(regex, query)
+        if search:
+            query = query.replace(search.group(), '')
+            url_parsed = url_parsed._replace(query=query)
+            url = urlunparse(url_parsed)
+
+        return url
